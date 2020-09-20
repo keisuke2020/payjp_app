1c40abf35afcc88e7a67858cb30c72349ac2a53b
keisuke2020 <kei_a_study@yahoo.co.jp>
2020-09-20 13:56:50 +0900

    userコントローラーを編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
  
   def show
+    Payjp.api_key = ENV["sk_test_96dbe48cc3a53f6dd17bc11d"] 
+    card = Card.find_by(user_id: current_user.id)
+
+    redirect_to new_card_path and return unless card.present?
+
+    customer = Payjp::Customer.retrieve(card.customer_token)
+    @card = customer.cards.first
   end
 
   def update
