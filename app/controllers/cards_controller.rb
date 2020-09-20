71f9982249aa667fc641d9ba3dcbe41d9db9dbca
keisuke2020 <kei_a_study@yahoo.co.jp>
2020-09-20 13:53:41 +0900

    cardコントローラーを設定

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,25 @@
 class CardsController < ApplicationController
+  
   def new
   end
+
+  def create
+    Payjp.api_key = ENV["sk_test_96dbe48cc3a53f6dd17bc11d"] 
+    customer = Payjp::Customer.create(
+    description: 'test', 
+    card: params[:card_token] 
+    )
+ 
+  card = Card.new( 
+    card_token: params[:card_token], 
+    customer_token: customer.id, 
+    user_id: current_user.id 
+  )
+    if card.save
+      redirect_to root_path
+    else
+      redirect_to "new" # カード登録画面
+    end
+  end  
+
 end
